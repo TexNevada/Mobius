2a5a501ecf895023e0574fdcfa7716239efb909b
Tex <15308761+texnevada@users.noreply.github.com>
2021-06-14 13:43:06 +0200

    Updated old MODUS file for Mobius overhaul

--- Mobius.py
+++ Mobius.py
@@ -30,8 +30,30 @@ import os
 import glob
 from datetime import date, datetime
 import random
+import configparser
 
 print(f"Discord.py version: {discord.__version__}")
+
+"""
+=========
+Config
+=========
+"""
+
+config = configparser.ConfigParser()
+config.read("./config.ini")
+
+APP = config["APP"]
+
+Environment = APP["ENVIRONMENT"]
+token = APP["TOKEN"]
+Debug = APP["DEBUG"]
+prefix = APP["PREFIX"]
+boot_msg = APP["BOOT_MSG"]
+
+log_location = config["LOGGING"]["LOG"]
+log_name = log_location+"Mobius.log"
+
 """
 =========
 Logging
@@ -41,7 +63,7 @@ Logging
 logs = False
 try:
     if os.path.exists("disablelogs.txt") is False:
-        files = glob.glob("discord.log")
+        files = glob.glob(log_name)
         now = datetime.now()
         time = now.strftime("%H.%M.%S")
         for file in files:
@@ -55,10 +77,14 @@ try:
 except:
     pass
 
+if Debug == "DEBUG":
+    LogLevel = logging.DEBUG
+else:
+    LogLevel = logging.DEBUG
 
 logger = logging.getLogger('discord')
-logger.setLevel(logging.WARNING)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+logger.setLevel(LogLevel)
+handler = logging.FileHandler(filename=log_name, encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
@@ -69,49 +95,19 @@ Prep files
 ===========
 """
 
-# opens token.json file on disk to be read. You will need to edit the token file for your own use.
-with open("token.json") as json_file:
-    # makes the name "data" a reference to the json file.
-    data = json.load(json_file)
-
-with open("prefix.json") as json_prefix:
-    # makes the name "prefix" a reference to the json file.
-    pre = json.load(json_prefix)
-
 
 def get_prefix(client, message):
-    try:
-        if message.guild:
-            # TODO: MongoDB or MySQL
-            if response["Prefix"] is not None:
-                CustomPrefix.append(response["Prefix"])
-                prefix = CustomPrefix
-            else:
-                prefix = pre["prefix"]
-            c.close()
-        else:
-            prefix = pre["prefix"]
-    except:
-        prefix = pre["prefix"]
-
     if not message.guild:
         return commands.when_mentioned_or(*prefix)(client, message)
-
-    # Allow users to @mention the bot instead of using a prefix when using a command. Also optional
-    # Do `return prefixes` if u don't want to allow mentions instead of prefix.
     return commands.when_mentioned_or(*prefix)(client, message)
 
 
-# Makes "token" a reference to the json file's value token
-token = data["token"]
-
 intents = discord.Intents.default()
 intents.members = True
-# This is the bot prefix. All "async def usercommand():" will always check if user has used the prefix
 client = commands.AutoShardedBot(
     command_prefix=get_prefix,
-    status=discord.Status.idle,
-    activity=discord.Game(name="Booting..."),
+    status=discord.Status.dnd,
+    activity=discord.Game(name=boot_msg),
     case_insensitive=True,
     # The total number of shards to use between all clusters
     # shard_count=4,
@@ -134,7 +130,9 @@ Functions checks
 def is_owner():
     # if the message from the user = his user ID then the user is owner
     async def predicate(ctx):
-        Authlist = [189490137762103298, 214200757992292353]
+        Authlist = []
+        for key, value in config["ADMIN"]:
+            Authlist.append(int(value))
         if ctx.author.id in Authlist:
             return True
     return commands.check(predicate)
@@ -160,17 +158,12 @@ async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=discord.Game(name="@MODUS help"))
     if os.path.exists("disablelogs.txt") is True:
         print("[WARNING]: Logging is disabled! To enable logging. Delete disablelogs.txt!")
-    client.db_Essential = MyDB("Essential")
-    client.db_Fallout = MyDB("Fallout")
-    client.db_Help = MyDB("Help")
-    # client.db_Logging = MyDB("Logging")
-    client.logo = "https://cdn.edb.tools/MODUS_Project/images/Enclave/EnclaveDatabase.png"
+
 
 # starts a client.event
 @client.event
 # Defines a fuction when a message is sent
 async def on_message(message):
-
     # Keeps the command in mind when event is running to prevent commands from not working
     await client.process_commands(message)
 
@@ -185,9 +178,9 @@ async def on_guild_join(guild):
 extension files loads here!
 ===========================
 """
-
+# TODO: Rebuilt cog loader
 # Here is all the extensions in the discord client. If a new extension is added. Add it here.
-cogs_list = ["cogs", "admin", "_Partners"]
+cogs_list = ["cogs", "admin"]
 
 if __name__ == "__main__":
     for load_dir in cogs_list:
@@ -201,75 +194,6 @@ if __name__ == "__main__":
 
 
 """
-==================
-Cogs / Extensions
-==================
-"""
-# TODO: Rewrite the reload command to proper unloading of cogs
-# client starts a command group
-@client.group(hidden=True)
-# checks to see if the user is the owner of the bot
-@is_owner()
-# Defines a function when the owner uses the reload command
-async def reload(ctx):
-    print(f"A reload command was executed in \"{ctx.guild.name}\" ")
-    # if no sub command in group is run. Run code.
-    if ctx.invoked_subcommand is None:
-        # Reloads a extension.
-        if __name__ == "__main__":
-            tempcoglist = []
-            # Will loop through the startup_extensions list
-            for extension in [f.replace('.py', '') for f in listdir(cogs_list[0]) if isfile(join(cogs_list[0], f))]:
-                # Code will try to unload the extensions & reload the extensions
-                try:
-                    # unloads extension
-                    cogToUnload = client.get_cog(extension)
-                    if cogToUnload:
-                        client.unload_extension(cogs_list[0] + "." + extension)
-                    # loads extension
-                    client.load_extension(cogs_list[0] + "." + extension)
-                    tempcoglist.append(extension)
-                # If extension fails to load. Execption error
-                except Exception as e:
-                    # Bot will shot itself
-                    await ctx.channel.send('\N{PISTOL}')
-                    # Bot replys with the problem
-                    await ctx.channel.send('{}: {}'.format(type(e).__name__, e))
-            # Prints out each extension it reloads
-            await ctx.send(f"Reloaded the following cogs:\n{tempcoglist}")
-
-# command recognizes its in a group.
-# will not run without main command being specified.
-@reload.command(hidden=True)
-# checks to see if the user is the owner of the bot
-@is_owner()
-# Defines a function when the owner uses the reload command
-async def admin(ctx):
-    print(f"A reload command was executed in \"{ctx.guild.name}\" ")
-    # Reloads a extension.
-    if __name__ == "__main__":
-        tempcoglist = []
-        # Will loop through the startup_extensions list
-        for extension in [f.replace('.py', '') for f in listdir(cogs_list[1]) if isfile(join(cogs_list[1], f))]:
-            # Code will try to unload the extensions & reload the extensions
-            try:
-                # unloads extension
-                cogToUnload = client.get_cog(extension)
-                if cogToUnload:
-                    client.unload_extension(cogs_list[1] + "." + extension)
-                # loads extension
-                client.load_extension(cogs_list[1] + "." + extension)
-                tempcoglist.append(extension)
-            # If extension fails to load. Execption error
-            except Exception as e:
-                # Bot will shot itself
-                await ctx.channel.send('\N{PISTOL}')
-                # Bot replys with the problem
-                await ctx.channel.send('{}: {}'.format(type(e).__name__, e))
-        # Prints out each extension it reloads
-        await ctx.send(f"Reloaded the following cogs:\n{tempcoglist}")
-
-"""
 ============================
 Logs the bot out of discord
 ============================
@@ -277,7 +201,7 @@ Logs the bot out of discord
 
 
 # logs out the bot from discord
-@client.command(name="logout", aliases=["shutdown", "kill-yourself"])
+@client.command(name="logout", aliases=["shutdown"])
 # User must have the role to continue
 @is_owner()
 # defines a new function call logout
