bc5ce352a9187219fbe0af71db64dd18ed2856a8
Tex <15308761+texnevada@users.noreply.github.com>
2021-06-24 08:13:19 +0200

    Cog loader is finished

--- data/cogs/_CogLoader.py
+++ data/cogs/_CogLoader.py
@@ -7,19 +7,43 @@ class _CogLoader(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
-    async def command(self, ctx):
-        await ctx.send("Stuff")
+        # Loads the cog at the beginning
+        cogs = os.listdir("./data/cogs/")
+        for item in ["_CogLoader.py", "__init__.py", "__pycache__"]:
+            cogs.remove(item)
+        errors = []
+        passed = []
+        passed_chk = False
+        for cog in cogs:
+            cog = cog.split(".")
+            try:
+                self.client.load_extension("data.cogs." + cog[0])
+                passed.append(cog[0])
+                passed_chk = True
+            except:
+                errors.append(cog[0])
+        if bool(errors):
+            for cog in errors:
+                print(f"ERROR! Could not load the following `{cog}`")
+        if passed_chk is True:
+            for cog in passed:
+                print(f"OK! Loaded {cog}")
 
-    @commands.command()
+    @commands.group(case_insensitive=True)
     @owner.is_owner()
-    async def reload(self, ctx):
-        print(f"A reload command was executed in \"{ctx.guild.name}\" ")
+    async def cog(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send("Available subcommands: reload, reload_all, load, unload")
+
+    @cog.command()
+    @owner.is_owner()
+    async def reload_all(self, ctx):
         cogs = os.listdir("./data/cogs/")
         for item in ["_CogLoader.py", "__init__.py", "__pycache__"]:
             cogs.remove(item)
         error = []
         passed = []
+        passed_chk = False
         for cog in cogs:
             cog = cog.split(".")
             try:
@@ -29,18 +53,71 @@ class _CogLoader(commands.Cog):
                     error.append(e)
                 self.client.load_extension("data.cogs."+cog[0])
                 passed.append(cog[0])
+                passed_chk = True
             except Exception as e:
                 error.append(e)
         if bool(error):
-            await ctx.send(error)
+            await ctx.send(f"ERROR! Could not load the following\n`{error}`")
+        if passed_chk is True:
+            await ctx.send(passed)
 
-        await ctx.send(passed)
-        # self.client.unload_extension()
-        # # loads extension
-        # self.client.load_extension()
-        # # If extension fails to load. Execption error
-        #
-        # await ctx.send(f"Refreshed the following cogs:\n{tempcoglist}")
+    @cog.command()
+    @owner.is_owner()
+    async def reload(self, ctx, arg: str = None):
+        if arg is None:
+            await ctx.send("No cog was given.")
+        else:
+            error = []
+            passed = False
+            try:
+                try:
+                    self.client.unload_extension("data.cogs." + arg)
+                except Exception as e:
+                    error.append(e)
+                self.client.load_extension("data.cogs." + arg)
+                passed = True
+            except Exception as e:
+                error.append(e)
+            if bool(error):
+                await ctx.send(error)
+            if passed is True:
+                await ctx.send(f"OK! Reloaded: {arg}")
+
+    @cog.command()
+    @owner.is_owner()
+    async def load(self, ctx, arg: str = None):
+        if arg is None:
+            await ctx.send("No cog was given.")
+        else:
+            error = []
+            passed = False
+            try:
+                self.client.load_extension("data.cogs." + arg)
+                passed = True
+            except Exception as e:
+                error.append(e)
+            if bool(error):
+                await ctx.send(error)
+            if passed is True:
+                await ctx.send(f"OK! Loaded: {arg}")
+
+    @cog.command()
+    @owner.is_owner()
+    async def unload(self, ctx, arg: str = None):
+        if arg is None:
+            await ctx.send("No cog was given.")
+        else:
+            error = []
+            passed = False
+            try:
+                self.client.unload_extension("data.cogs." + arg)
+                passed = True
+            except Exception as e:
+                error.append(e)
+            if bool(error):
+                await ctx.send(error)
+            if passed is True:
+                await ctx.send(f"OK! Unloaded: {arg}")
 
 
 def setup(client):
