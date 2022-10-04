79b6735f0823e220c63248141a7647a34de4461b
Christer G. Sørensen <15308761+texnevada@users.noreply.github.com>
2022-10-04 05:40:39 +0200

    Added FED commands

--- data/cogs/Partner_F76_FED76.py
+++ data/cogs/Partner_F76_FED76.py
@@ -2,12 +2,78 @@
 import discord
 # Imports commands
 from discord.ext import commands
+from discord import app_commands
 import requests
+import configparser
 
 
 class fed76(commands.Cog):
-    def __init__(self, client):
+    def __init__(self, client: commands.Bot) -> None:
         self.client = client
+        super().__init__()
+
+    @app_commands.command(name="fed76", description="Look up Fallout 76 weapon prices on FED76!")
+    async def fed(self, interaction: discord.Interaction, item: str, mods: str, grade: str = None):
+        r = requests.get(f"https://fed76.info/pricing-api/?item={item}&mods={mods}&grade={grade}")
+        try:
+            x = r.json()
+            embed = discord.Embed(color=0xc7974b, title="FED Legendary Item Value Estimator",
+                                  url="https://fed76.info/i/item-pricing-tool")
+            embed.set_footer(text="Experienced traders can buy cheaper and sell higher")
+            embed.set_thumbnail(url=x["review"]["author"]["logo"])
+            embed.add_field(name=f"__**{x['category']}**__", value=x['name'], inline=True)
+            embed.add_field(name="__**FED Rating**__",
+                            value=f"{x['review']['reviewRating']['ratingValue']}/5.0", inline=True)
+            # embed.add_field(name="__**Price range**__", value=x['price'], inline=False)
+            embed.add_field(name="__**Recommendation**__", value=x['review']['description'], inline=False)
+
+            # Embed image
+            # file = discord.File(f"items/images/Fed76/HowToMobile.jpg", filename=f"HowToMobile.jpg")
+            # embed.set_image(url=f"attachment://HowToMobile.jpg")
+            embed.set_image(url="https://fed76.s3.eu-west-2.amazonaws.com/HowToPricing.jpg")
+            await interaction.response.send_message(embed=embed)
+        except Exception as e:
+            print("==============FED76=ERROR==============")
+            print(f"Request returned: {r.status_code}")
+            print("Most of the time this is a user input error")
+            print(e)
+            print("==================END==================")
+            await interaction.response.send_message("There seems to have been a connection error to FED76. Please try again later")
+
+    @app_commands.command(name="fed76info", description="Look up information about weapons & items!")
+    async def fed_info(self, interaction: discord.Interaction, item: str, mods: str = None, grade: str = None):
+        print(f"A user requested the FED76INFO command")
+
+        if mods is None:
+            mods = ""
+        if grade is None:
+            grade = ""
+        r = requests.get(f"https://fed76.info/pricing/explain/?item={item}&mods={mods}&grade={grade}")
+        try:
+            x = r.json()
+            embed = discord.Embed(color=0xc7974b, title="FED76 - Item Pricing Tool for Fallout 76",
+                                  url="https://fed76.info/i/item-pricing-tool")
+            embed.set_footer(text="")
+            embed.set_thumbnail(url='https://fed76.info/static/fed150.png')
+            embed.add_field(name=f"__**{x['names'][0]}**__", value=x['itemDescription'], inline=False)
+            for i in range(len(x['names'][1:])):
+                conditional = x['effectConditionalDescriptions'][i]
+                value = f"{x['effectDescriptions'][i]}\n```{conditional}```" if conditional else f"{x['effectDescriptions'][i]}"
+                embed.add_field(name=f"__**{x['names'][i + 1]}**__", value=value, inline=True)
+            # embed.add_field(name="__**FED Rating**__", value=f"{x['rating']}/5.0", inline=False)
+
+            # Bot runs the embed
+            await interaction.response.send_message(embed=embed)
+        except Exception as e:
+            print("==============FED76=ERROR==============")
+            print(f"Request returned: {r.status_code}")
+            print("Most of the time this is a user input error")
+            print(e)
+            print("==================END==================")
+
+            await interaction.response.send_message("Hmm. That didn't seem to work. Check abbreviations here: "
+                                                    "https://fed76.info/i/discord-abbreviations/")
+
 
     @commands.command(aliases=["fed"])
     async def fed76(self, ctx, arg1=None, arg2=None, arg3=None):
@@ -15,6 +81,9 @@ class fed76(commands.Cog):
             print(f"A user requested the FED76 command in \"{ctx.guild.name}\"")
         else:
             print(f"A user requested the FED76 command in a private message")
+        config = configparser.ConfigParser()
+        config.read("./config.ini")
+        await ctx.send(config["LEGACY"]["UseSlash"])
         if arg2 is None:
             await ctx.send("This tool is meant as a quick check on prices before you make your judgement on selling or "
                            "buying.\n"
@@ -63,6 +132,9 @@ class fed76(commands.Cog):
             print(f"A user requested the FED76INFO command in \"{ctx.guild.name}\"")
         else:
             print(f"A user requested the FED76INFO command in a private message")
+        config = configparser.ConfigParser()
+        config.read("./config.ini")
+        await ctx.send(config["LEGACY"]["UseSlash"])
         if arg1 is None:
             await ctx.send(
                 "This tool provides additional information regarding legendary items, effects, and how they go together..\n"
@@ -104,5 +176,5 @@ class fed76(commands.Cog):
 
 
 # ends the extension
-def setup(client):
-    client.add_cog(fed76(client))
+async def setup(client) -> None:
+    await client.add_cog(fed76(client))
