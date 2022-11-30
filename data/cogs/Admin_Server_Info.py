f26d3e012114505f88a19283f4d54895059ac057
Christer G. Sørensen <15308761+texnevada@users.noreply.github.com>
2022-11-30 04:53:50 +0100

    Fixed several issues with serverinfo command

--- data/cogs/Admin_Server_Info.py
+++ data/cogs/Admin_Server_Info.py
@@ -27,11 +27,24 @@ class Admin_Server_Info(commands.Cog):
         print(f"The serverinfo command was requested in {ctx.guild.name}")
         try:
             embed = discord.Embed(title=f"Server information for {ctx.guild.name}", color=discord.Color.red())
-            embed.set_thumbnail(url=ctx.guild.icon_url)
+            embed.set_thumbnail(url=ctx.guild.icon.url)
             # embed.set_author(name=f"")
             embed.add_field(name="Server owner", value=ctx.guild.owner, inline=True)
-            embed.add_field(name="Region", value=ctx.guild.region, inline=True)
+            # embed.add_field(name="Region", value=ctx.guild.region, inline=True)
             embed.add_field(name="Server ID", value=ctx.guild.id, inline=True)
+            embed.add_field(name="Upload limit", value=f"`{convert_size(ctx.guild.filesize_limit)}`", inline=True)
+
+            embed.add_field(name="Emoji limit", value=f"`{ctx.guild.emoji_limit}`", inline=True)
+            embed.add_field(name="Sticker limit", value=f"`{ctx.guild.sticker_limit}`", inline=True)
+
+            try:
+                bans = 0
+                for x in await ctx.guild.bans():
+                    bans += 1
+                embed.add_field(name="Bans", value=f"`{bans}`", inline=True)
+            except:
+                embed.add_field(name="Bans", value="Missing ban perm", inline=True)
+
             channels = len(ctx.guild.channels) - len(ctx.guild.categories)
             embed.add_field(name="Channels",
                             value=f"Text channels: `{len(ctx.guild.text_channels)}`\n"
@@ -59,16 +72,7 @@ class Admin_Server_Info(commands.Cog):
                                 inline=True)
             except:
                 embed.add_field(name="Prune estimate", value="Missing kick perm", inline=True)
-            embed.add_field(name="Emoji limit", value=f"`{ctx.guild.emoji_limit}`", inline=True)
-            uploadlimit = convert_size(ctx.guild.filesize_limit)
-            embed.add_field(name="Upload limit", value=f"`{uploadlimit}`", inline=True)
-            try:
-                bans = 0
-                for x in await ctx.guild.bans():
-                    bans += 1
-                embed.add_field(name="Bans", value=f"`{bans}`", inline=True)
-            except:
-                embed.add_field(name="Bans", value="Missing ban perm", inline=True)
+
             if ctx.guild.premium_tier != 0:
                 embed.add_field(name="Server's Nitro Tier", value=f"`{ctx.guild.premium_tier}`", inline=True)
             if ctx.guild.premium_subscription_count != 0:
@@ -77,12 +81,23 @@ class Admin_Server_Info(commands.Cog):
                 embed.add_field(name="\uFEFF", value="\uFEFF", inline=True)
             if ctx.guild.features is False:
                 embed.add_field(name="Server features", value=ctx.guild.features, inline=True)
-
-            x = datetime.datetime.strptime(str(ctx.guild.created_at), "%Y-%m-%d %H:%M:%S.%f")
+            year = ctx.guild.created_at.year
+            month = ctx.guild.created_at.month
+            day = ctx.guild.created_at.day
+            hour = ctx.guild.created_at.hour
+            minute = ctx.guild.created_at.minute
+            second = ctx.guild.created_at.second
+            x = datetime.datetime.strptime(str(f"{year}-{month}-{day} {hour}:{minute}:{second}"), "%Y-%m-%d %H:%M:%S")
             x = x.strftime("%d %B %Y\n%H:%M:%S UTC")
             embed.add_field(name="Server created at", value=f"{x}", inline=True)
 
-            x = datetime.datetime.strptime(str(ctx.guild.me.joined_at), "%Y-%m-%d %H:%M:%S.%f")
+            year = ctx.guild.me.joined_at.year
+            month = ctx.guild.me.joined_at.month
+            day = ctx.guild.me.joined_at.day
+            hour = ctx.guild.me.joined_at.hour
+            minute = ctx.guild.me.joined_at.minute
+            second = ctx.guild.me.joined_at.second
+            x = datetime.datetime.strptime(str(f"{year}-{month}-{day} {hour}:{minute}:{second}"), "%Y-%m-%d %H:%M:%S")
             x = x.strftime("%d %B %Y\n%H:%M:%S UTC")
             embed.add_field(name="Bot joined date", value=f"{x}", inline=True)
 
@@ -111,20 +126,20 @@ class Admin_Server_Info(commands.Cog):
     # async def auditlogs(self, ctx):
     #     return None
 
-    @commands.command()
-    @commands.has_permissions(manage_channels=True)
-    @commands.guild_only()
-    async def modactions(self, ctx, member: discord.Member = None):
-        print(f"The modactions command was requested in {ctx.guild.name}")
-        try:
-            if member is None:
-                member = ctx.author
-            entries = await ctx.guild.audit_logs(limit=None, user=member).flatten()
-            await ctx.send(f'This user has made {len(entries)} moderation actions.')
-        except:
-            await ctx.send("I don't have access to view the audit logs. "
-                           "Access can be gained by enabling Administrator or "
-                           "View Audit Logs setting in the MODUS role")
+    # @commands.command()
+    # @commands.has_permissions(manage_channels=True)
+    # @commands.guild_only()
+    # async def modactions(self, ctx, member: discord.Member = None):
+    #     print(f"The modactions command was requested in {ctx.guild.name}")
+    #     try:
+    #         if member is None:
+    #             member = ctx.author
+    #         entries = await ctx.guild.audit_logs(limit=None, user=member).flatten()
+    #         await ctx.send(f'This user has made {len(entries)} moderation actions.')
+    #     except:
+    #         await ctx.send("I don't have access to view the audit logs. "
+    #                        "Access can be gained by enabling Administrator or "
+    #                        "View Audit Logs setting in the MODUS role")
 
 
 async def setup(client: commands.Bot) -> None:
