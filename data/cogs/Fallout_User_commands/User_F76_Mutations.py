4742a9169b6fe0a5059eb9a12a7721453e3e845e
Christer G. Sørensen <15308761+texnevada@users.noreply.github.com>
2023-10-24 08:41:55 +0200

    Removed spell checker and disabled old command

--- data/cogs/Fallout_User_commands/User_F76_Mutations.py
+++ data/cogs/Fallout_User_commands/User_F76_Mutations.py
@@ -2,14 +2,10 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 from typing import Literal
-from spellchecker import SpellChecker
 from data.functions.MySQL_Connector import MyDB
 import configparser
 from data.functions.logging import get_log
 logger = get_log(__name__)
-
-spell = SpellChecker()
-spell.word_frequency.load_text_file("./data/cogs_data/F76_Mutation_Word_List.txt")
 
 
 # start of the extension as a class.
@@ -59,96 +55,11 @@ class User_F76_Mutations(commands.Cog):
         c.close()
 
     @commands.command()
-    async def mutation(self, ctx, *, arg: str):
-        if ctx.guild:
-            logger.info(f"A user requested the mutation command in \"{ctx.guild.name}\"")
-        else:
-            logger.info(f"A user requested the mutation command in a private message")
+    async def mutation(self, ctx):
+        logger.info(f"A user requested a legacy command")
         config = configparser.ConfigParser()
         config.read("./config.ini")
         await ctx.send(config["LEGACY"]["UseSlash"])
-        c = MyDB("fallout")
-        c.execute("SELECT * FROM en_Mutations WHERE Mutation LIKE %s", ('%{}%'.format(arg),))
-        async with ctx.channel.typing():
-            response = c.fetchone()
-            if not response:
-                if len(arg) < 15:
-                    async with ctx.channel.typing():
-                        logger.info(f"Checking spell checker with the following word: {arg}")
-                        entryNR = 0
-                        for entry in spell.candidates(arg):
-                            entryNR = entryNR + 1
-                            c.execute("SELECT * FROM en_Mutations WHERE Mutation LIKE %s", ('%{}%'.format(entry),))
-                            response = c.fetchone()
-                            if response or entryNR == 3:
-                                break
-
-            if response:
-                try:
-                    embed = discord.Embed(color=0x28df28, title=f"The {response['Mutation']} Mutation")
-
-                    # embed.set_footer(text="This was brought to you by Enclave Database automated reply system")
-                    # embed.set_image(url="https://i.imgur.com/XaNIjHG.png")
-
-                    if response["ImageURL"]:
-                        # embed.set_thumbnail(url=response["ImageURL"])
-                        embed.set_image(url=response["ImageURL"])
-                    # embed.set_author(name=f"The {response['Mutation']} Mutation")
-                    embed.set_thumbnail(url=config["EDB.TOOLS"]["F76_Mutation"])
-
-                    embed.add_field(name="__**Mutation effects**__",
-                                    value=f"**Positive effects:** {response['PositiveEffects']}\n"
-                                          f"**Negative effects:** {response['NegativeEffects']}",
-                                    inline=False)
-                    # Adding as space between fields
-                    # embed.add_field(name="\uFEFF", value="\uFEFF", inline=False)
-                    embed.add_field(
-                        name=f"__**{response['Mutation']} serum can by doing the following requirements**__",
-                        value=f"**Requirements:** {response['Requirements']}\n"
-                              f"**Materials:** {response['Materials']}",
-                        inline=False)
-                    embed.set_footer(text="Special thanks to Shia for the art!\n"
-                                          "You can find him here: https://www.deviantart.com/overshia")
-                    # Bot runs the embed
-                    await ctx.send(embed=embed)
-                # Will report to the player if what they typed doesn't exist within the database
-                except TypeError as e:
-                    await ctx.send("Could not find anything by that name. Did you type it correctly?")
-                    logger.info(e)
-                # Will throw this error if something else went wrong
-                except Exception as e:
-                    await ctx.send("Something went very wrong here. Contact support here https://discord.gg/hMfgSaN!")
-                    logger.info(e)
-            else:
-                await ctx.send("Could not find anything by that name. Did you type it correctly?")
-        c.close()
-
-    # @commands.command()
-    # async def mutations(self, ctx):
-    #     if ctx.guild:
-    #         logger.info(f"A user requested the mutations command in \"{ctx.guild.name}\"")
-    #     else:
-    #         logger.info(f"A user requested the mutations command in a private message")
-    #     config = configparser.ConfigParser()
-    #     config.read("./config.ini")
-    #     c = MyDB("fallout")
-    #     c.execute("SELECT * FROM en_Mutations")
-    #     embed = discord.Embed(color=0x28df28, title=f"All of Fallout 76 mutations!")
-    #     embed.set_thumbnail(url=config["EDB.TOOLS"]["F76_Mutations"])
-    #     async with ctx.channel.typing():
-    #         try:
-    #             response = c.fetchall()
-    #             for mutation in response:
-    #                 embed.add_field(name=mutation['Mutation'],
-    #                                 value=f"**Positive:** {mutation['PositiveEffects']}\n"
-    #                                       f"**Negative:** {mutation['NegativeEffects']}",
-    #                                 inline=False)
-    #             await ctx.send(embed=embed)
-    #         except Exception as e:
-    #             await ctx.send("Looks like there might be some connection issues with the database. "
-    #                            "Please try again later")
-    #             logger.info(e)
-    #     c.close()
 
 
 # ends the extension
