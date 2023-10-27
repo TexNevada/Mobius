27f981ad6ae55aa659ef3cf062a6e65055a6b0f1
Christer G. Sørensen <15308761+texnevada@users.noreply.github.com>
2023-10-27 12:23:01 +0200

    Updated req installer to support cog requirements

--- data/functions/ReqInstaller.py
+++ data/functions/ReqInstaller.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import subprocess
 
@@ -5,18 +6,34 @@ import subprocess
 def installer():
     print("[INFO]: Installing requirements")
     subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+    print("[INFO]: Checking for any additional requirements by the cogs")
+    install_requirements_in_directory("data/cogs")
     print("\n[INFO]: Rerun the program to continue"
           "\n[INFO]: Exiting...")
     sys.exit()
 
 
+def install_requirements_in_directory(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file == 'requirements.txt':
+                requirements_path = os.path.join(root, file)
+                installer_path = os.path.join(root, 'installer.py')
+                if os.path.exists(installer_path):
+                    print(f"[INFO]: Installing requirements in {requirements_path}")
+                    subprocess.check_call([sys.executable, installer_path])
+                else:
+                    print(f"[INFO]: Installer not found in {root}")
+
+
 def check():
-    try:
-        import discord
-        import aiohttp
-        import requests
-        import mysql.connector
-        import colorama
-        import topgg
-    except ImportError:
-        installer()
+    # try:
+    #     import discord
+    #     import aiohttp
+    #     import requests
+    #     import mysql.connector
+    #     import colorama
+    #     import topgg
+    # except ImportError:
+    #     installer()
+    installer()
