1f55b9867a21f3b21741fe5fd27158031a80fa64
Christer G. Sørensen <15308761+texnevada@users.noreply.github.com>
2023-11-10 08:36:36 +0100

    Updated heartbeat.py

--- data/functions/heartbeat.py
+++ data/functions/heartbeat.py
@@ -1,6 +1,9 @@
 import aiohttp
 import configparser
 import asyncio
+from data.functions.logging import get_log
+logger = get_log(__name__)
+
 """
 =================================================
 Sends heartbeats to monitoring service
@@ -15,5 +18,6 @@ config.read("./config.ini")
 async def heartbeat():
     while True:
         async with aiohttp.ClientSession() as session:
-            async with session.get(config["Logging"]["heartbeat_url"]):
+            async with session.get(config["Logging"]["heartbeat_url"]) as response:
+                logger.debug(f"Heartbeat to origin returned: {response.status}")
                 await asyncio.sleep(60)
