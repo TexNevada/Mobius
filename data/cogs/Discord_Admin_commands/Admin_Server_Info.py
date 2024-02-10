a9ed4dc8ee6859e616bdc94adc20b7f68066b086
Christer G. Sørensen <15308761+texnevada@users.noreply.github.com>
2024-02-10 04:00:08 +0100

    Overhauled the serverinfo command

--- data/cogs/Discord_Admin_commands/Admin_Server_Info.py
+++ data/cogs/Discord_Admin_commands/Admin_Server_Info.py
@@ -1,6 +1,7 @@
 import discord
 import math
 import datetime
+import time
 from discord.ext import commands
 from data.functions.logging import get_log
 
@@ -30,27 +31,31 @@ class Admin_Server_Info(commands.Cog):
             embed = discord.Embed(title=f"Server information for {ctx.guild.name}", color=discord.Color.red())
             embed.set_thumbnail(url=ctx.guild.icon.url)
             # embed.set_author(name=f"")
-            embed.add_field(name="Server owner", value=ctx.guild.owner, inline=True)
+            embed.add_field(name="Server owner", value=f"{ctx.guild.owner.global_name}\n", inline=True)
             # embed.add_field(name="Region", value=ctx.guild.region, inline=True)
-            embed.add_field(name="Server ID", value=ctx.guild.id, inline=True)
-            embed.add_field(name="Upload limit", value=f"`{convert_size(ctx.guild.filesize_limit)}`", inline=True)
+            embed.add_field(name="Server ID", value=f"`{ctx.guild.id}`", inline=True)
+            # embed.add_field(name="\uFEFF", value="\uFEFF", inline=True)
 
-            embed.add_field(name="Emoji limit", value=f"`{ctx.guild.emoji_limit}`", inline=True)
-            embed.add_field(name="Sticker limit", value=f"`{ctx.guild.sticker_limit}`", inline=True)
 
-            try:
-                bans = 0
-                for x in await ctx.guild.bans():
-                    bans += 1
-                embed.add_field(name="Bans", value=f"`{bans}`", inline=True)
-            except:
-                embed.add_field(name="Bans", value="Missing ban perm", inline=True)
+            embed.add_field(name="Bot info", value=f"Shard ID: `{ctx.guild.shard_id}`", inline=True)
+
+
+            # embed.add_field(name="Upload limit", value=f"`{convert_size(ctx.guild.filesize_limit)}`", inline=True)
+            embed.add_field(name="Server Limits", value=f"Emoji: `{ctx.guild.emoji_limit}`\n"
+                                                        f"Sticker: `{ctx.guild.sticker_limit}`\n"
+                                                        f"Upload: `{convert_size(ctx.guild.filesize_limit)}`")
+            # embed.add_field(name="Emoji limit", value=f"`{ctx.guild.emoji_limit}`", inline=True)
+            # embed.add_field(name="Sticker limit", value=f"`{ctx.guild.sticker_limit}`", inline=True)
+
+
 
             channels = len(ctx.guild.channels) - len(ctx.guild.categories)
+            total = channels + len(ctx.guild.threads)
             embed.add_field(name="Channels",
                             value=f"Text channels: `{len(ctx.guild.text_channels)}`\n"
                                   f"Voice channels: `{len(ctx.guild.voice_channels)}`\n"
-                                  f"Total: `{channels}`",
+                                  f"Threads: `{len(ctx.guild.threads)}`\n"
+                                  f"Total: `{total}`",
                             inline=True)
             members = set(ctx.guild.members)
             bots = filter(lambda m: m.bot, members)
@@ -59,8 +64,25 @@ class Admin_Server_Info(commands.Cog):
             embed.add_field(name="Members",
                             value=f"Users: `{len(users)}`\n"
                                   f"Bots: `{len(bots)}`\n"
-                                  f"Total: `{ctx.guild.member_count}`",
+                                  f"Total: `{ctx.guild.member_count}/{ctx.guild.max_members}`",
                             inline=True)
+
+            #embed.add_field(name="\uFEFF", value="\uFEFF", inline=True)
+
+            embed.add_field(name="Server boost status",
+                            value=f"Boost Tier: `{ctx.guild.premium_tier}`\n"
+                                  f"Subscriber count: `{ctx.guild.premium_subscription_count}`", inline=True)
+
+            features = '\n'.join(item.capitalize() for item in ctx.guild.features)
+            embed.add_field(name="Server features", value=features, inline=True)
+
+            # try:
+            #     bans = 0
+            #     for x in await ctx.guild.bans():
+            #         bans += 1
+            #     embed.add_field(name="Bans", value=f"`{bans}`", inline=True)
+            # except:
+            #     embed.add_field(name="Bans", value="Missing ban perm", inline=True)
 
             try:
                 estimate = await ctx.guild.estimate_pruned_members(days=1)
@@ -74,23 +96,14 @@ class Admin_Server_Info(commands.Cog):
             except:
                 embed.add_field(name="Prune estimate", value="Missing kick perm", inline=True)
 
-            if ctx.guild.premium_tier != 0:
-                embed.add_field(name="Server's Nitro Tier", value=f"`{ctx.guild.premium_tier}`", inline=True)
-            if ctx.guild.premium_subscription_count != 0:
-                embed.add_field(name="Total server boosts", value=f"`{ctx.guild.premium_subscription_count}`",
-                                inline=True)
-                embed.add_field(name="\uFEFF", value="\uFEFF", inline=True)
-            if ctx.guild.features is False:
-                embed.add_field(name="Server features", value=ctx.guild.features, inline=True)
             year = ctx.guild.created_at.year
             month = ctx.guild.created_at.month
             day = ctx.guild.created_at.day
             hour = ctx.guild.created_at.hour
             minute = ctx.guild.created_at.minute
             second = ctx.guild.created_at.second
-            x = datetime.datetime.strptime(str(f"{year}-{month}-{day} {hour}:{minute}:{second}"), "%Y-%m-%d %H:%M:%S")
-            x = x.strftime("%d %B %Y\n%H:%M:%S UTC")
-            embed.add_field(name="Server created at", value=f"{x}", inline=True)
+
+            unix_time_created_at = time.mktime(datetime.datetime(year, month, day, hour, minute, second).timetuple())
 
             year = ctx.guild.me.joined_at.year
             month = ctx.guild.me.joined_at.month
@@ -98,20 +111,24 @@ class Admin_Server_Info(commands.Cog):
             hour = ctx.guild.me.joined_at.hour
             minute = ctx.guild.me.joined_at.minute
             second = ctx.guild.me.joined_at.second
-            x = datetime.datetime.strptime(str(f"{year}-{month}-{day} {hour}:{minute}:{second}"), "%Y-%m-%d %H:%M:%S")
-            x = x.strftime("%d %B %Y\n%H:%M:%S UTC")
-            embed.add_field(name="Bot joined date", value=f"{x}", inline=True)
 
-            if ctx.guild.mfa_level == 0:
+            unix_time = time.mktime(datetime.datetime(year, month, day, hour, minute, second).timetuple())
+
+            # embed.add_field(name="\uFEFF", value="\uFEFF", inline=True)
+
+            embed.add_field(name="Server created at", value=f"<t:{int(unix_time_created_at)}:f>\n", inline=True)
+            embed.add_field(name="Bot joined date", value=f"<t:{int(unix_time)}:f>", inline=True)
+
+            if ctx.guild.mfa_level.value == 0:
                 embed.add_field(name="2 Factor Authentication Security",
-                                value="**2FA Setting: OFF!!**\n"
-                                      "We highly recommend turning on 2FA on the server for good security\n"
-                                      "This prevents any hackers who might have access to any compromised admin account"
-                                      " from being able to administer the server.",
+                                value="**2FA Setting: `OFF`**\n"
+                                      "We highly recommend turning on 2FA on the server for good security. "
+                                      "This prevents hackers with access to compromised admin accounts "
+                                      "from changing any server settings without a 2FA code.",
                                 inline=False)
-            elif ctx.guild.mfa_level == 1:
+            elif ctx.guild.mfa_level.value == 1:
                 embed.add_field(name="2 Factor Authentication Security",
-                                value="**2FA Setting: ON!**\n"
+                                value="**2FA Setting: `ON`**\n"
                                       "You are protected against compromised admin accounts!\n"
                                       "__This does not protect against compromised bot accounts.__",
                                 inline=False)
